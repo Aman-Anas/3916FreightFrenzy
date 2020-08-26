e087a25e6da7ec4d3925dd76c40aace7417120e0
Aman Anas <41600372+Aman-Anas@users.noreply.github.com>
2020-08-26 15:32:48 -0400

    Added FTCLib TeleOp mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCLibRobotFunctions.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.SimpleMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class FTCLibRobotFunctions extends FTCLibDriveCode {
+    /**
+     * Put extra game-specific robot functionality here,
+     * such as additional motors, servos, and sensors for arms, claws, and lifts.
+     */
+
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_FTCLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_FTCLib.java
@@ -1,49 +1,76 @@
 /*
 Apex Robotics FTC Team 3916: Main TeleOp for SkyStone season (2019-2020)
 
-Uses a Mechanum-style drivetrain for movement.
+Uses a Mecanum-style drivetrain for movement.
  */
 
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="Basic TeleOp", group="Apex Robotics 3916")
+@TeleOp(name="FTCLib TeleOp", group="Apex Robotics 3916")
 //@Disabled
-public class TeleOp_FTCLib extends OpMode {
+public class TeleOp_FTCLib extends LinearOpMode {
 
-    private Robot bot = new Robot();
-
-    @Override
-    public void init() {
-        bot.init(hardwareMap, Robot.DriveType.Mechanum);
-    }
+    private FTCLibRobotFunctions bot = new FTCLibRobotFunctions();
 
     @Override
-    public void loop() {
+    public void runOpMode() throws InterruptedException {
+        //Initialize code and variables
+
+        bot.init(hardwareMap, FTCLibRobotFunctions.DriveType.Mecanum);
+
+        GamepadEx Gamepad1 = new GamepadEx(gamepad1);
+        GamepadEx Gamepad2 = new GamepadEx(gamepad2);
+
         final double STICK_DEAD_ZONE = 0.1;
-
         double x = 0;
         double y = 0;
-        if (gamepad1.left_bumper) {
-            bot.mech_rotate(0);
-            telemetry.addData("Status", "Rotating Counterclockwise");
-            telemetry.update();
-        } else if (gamepad1.right_bumper) {
-            bot.mech_rotate(1);
-            telemetry.addData("Status", "Rotating Clockwise");
-            telemetry.update();
-        } else {
-            if (Math.abs(gamepad1.left_stick_y) > STICK_DEAD_ZONE) {
-                y = gamepad1.left_stick_y;
+        double z = 0;
+
+        //Wait for the driver to hit Start
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+            if (Gamepad1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.1) {
+                //update z with left trigger, negative since left
+                z = -1 * Gamepad1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER);
+
+            } else if (Gamepad1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.1 ) {
+                //update z with right trigger
+                z = Gamepad1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+
+            } else {
+                z = 0;
             }
-            if (Math.abs(gamepad1.left_stick_x) > STICK_DEAD_ZONE) {
-                x = gamepad1.left_stick_x;
+
+            if (Math.abs(Gamepad1.getLeftY()) > STICK_DEAD_ZONE) {
+                //update y with current y position
+                y = Gamepad1.getLeftY();
+            } else {
+                y = 0;
             }
-            MechPower pwr = bot.mech_drive(x, y);
-            telemetry.addData("Status", "power: x:" + x + " y:" + y + " =pwr:" + pwr.toString());
+            if (Math.abs(Gamepad1.getLeftX()) > STICK_DEAD_ZONE) {
+                //update x with current x position
+                x = Gamepad1.getLeftX();
+            } else {
+                x = 0;
+            }
+
+            //Send the X, Y, and rotation (Z) to the mecanum method
+            bot.mecanumDrivetrain.driveRobotCentric(x,y,z);
+
+            //Add a little telemetry
+            telemetry.addData("Status", "power: x:" + x + " y:" + y + " z:" + z);
             telemetry.update();
+
+
         }
     }
 }
