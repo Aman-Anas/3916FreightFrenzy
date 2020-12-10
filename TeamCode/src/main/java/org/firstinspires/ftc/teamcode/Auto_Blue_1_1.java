d6620172f156cdbda6faea7693905e189da5dda6
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2020-12-10 15:21:21 -0500

    Added auto blue 1 1-3 to code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_1_1.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name="Auto Blue 1 1", group="Apex Robotics 3916")
+public class Auto_Blue_1_1 extends LinearOpMode {
+    //Add automous stuff here.
+    //https://www.learnroadrunner.com/
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Trajectory traj = drive.trajectoryBuilder(new Pose2d(-62.0, 50, 0), 0)
+                .splineToSplineHeading(new Pose2d(-62.01, 50), Math.toRadians(10))
+                .splineToSplineHeading(new Pose2d(50, 60), 0)
+                .splineToSplineHeading(new Pose2d(10, 60), 0)
+                .build();
+
+        drive.followTrajectory(traj);
+
+        sleep(2000);
+    }
+}
