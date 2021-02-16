6746084e16e2a092184429e7d4362a3028ca1b1a
Aman Anas <IA.AmanAnas@gmail.com>
2021-02-16 15:48:18 -0500

    Fix length of 4-ring auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_1_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_1_1.java
@@ -22,7 +22,7 @@ public class Auto_Red_1_1 extends LinearOpMode {
         drive.setPoseEstimate(startPose);
         Trajectory traj = drive.trajectoryBuilder(new Pose2d(-62.0, -50, 0), 0)
                 .splineToSplineHeading(new Pose2d(-61.99, -50, 0.0), Math.toRadians(-10.0))
-                .splineToSplineHeading(new Pose2d(50.0, -60.0), 0.0)
+                .splineToSplineHeading(new Pose2d(55.0, -60.0), 0.0)
                 .splineToSplineHeading(new Pose2d(10.0, -60.0), 0.0)
                 .build();
 
