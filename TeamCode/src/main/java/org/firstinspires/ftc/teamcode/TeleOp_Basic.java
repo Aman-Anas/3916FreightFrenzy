fbdad1b4715e6387599547558be9e314ad0504d4
tubaDude99 <63601867+tubaDude99@users.noreply.github.com>
2021-10-03 17:48:32 -0400

    Update TeleOp_Basic.java
    Added code to adjust inputs to correct for the dead zone.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Basic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Basic.java
@@ -37,7 +37,7 @@ public class TeleOp_Basic extends LinearOpMode {
         GamepadEx Gamepad1 = new GamepadEx(gamepad1);
         //GamepadEx Gamepad2 = new GamepadEx(gamepad2);
 
-        double STICK_DEAD_ZONE = TeleOpConfig.STICK_DEAD_ZONE;
+        //double STICK_DEAD_ZONE = TeleOpConfig.STICK_DEAD_ZONE;
         double x = 0;
         double y = 0;
         double z = 0;
@@ -54,6 +54,7 @@ public class TeleOp_Basic extends LinearOpMode {
             /*
                 GAMEPAD 1
             */
+            //Get stick inputs
             double leftY = Gamepad1.getLeftY();
             double leftX = Gamepad1.getLeftX();
             double rightX = Gamepad1.getRightX();
@@ -67,21 +68,33 @@ public class TeleOp_Basic extends LinearOpMode {
                 turnMultiplier = 1;
             }
 
+            //This next section uses some janky math magic to correct for the dead zone. Feel free to simplify it if you can.
+            /*
+                How it works:
+                    First, it removes the dead zone, either subtracting it (from a positive input) or adding it (to a negative input).
+                    It does this by multiplying the dead zone by the input divided by the abs val of the input, effectively multiplying by 1 for a positive input,
+                        or multiplying by -1 for a negative input.
+                    After removing the dead zone, it rescales the value by dividing by 1 minus the dead zone, which ensures that full inputs produce full power,
+                        zero value inputs (at or below the dead zone) are at zero, and everything in between is scaled proportionally.
+                    Direction inversions and Precision Mode are implemented after this correction for simplicity.
+             */
             if (Math.abs(rightX) > TeleOpConfig.STICK_DEAD_ZONE) {
-                z = rightX * turnMultiplier;
+                z = (rightX - (TeleOpConfig.STICK_DEAD_ZONE * rightX / Math.abs(rightX))) / (1.0 - TeleOpConfig.STICK_DEAD_ZONE);
+                z = z * turnMultiplier;
             } else {
                 z = 0;
             }
 
             if (Math.abs(leftY) > TeleOpConfig.STICK_DEAD_ZONE) {
-                y = -(leftY) * powerMultiplier;
+                y = (leftY - (TeleOpConfig.STICK_DEAD_ZONE * leftY / Math.abs(leftY))) / (1.0 - TeleOpConfig.STICK_DEAD_ZONE);
+                y = -(y) * powerMultiplier;
             } else {
                 y = 0;
             }
 
             if (Math.abs(leftX) > TeleOpConfig.STICK_DEAD_ZONE) {
-                //update x with current x position
-                x = -(leftX) * powerMultiplier;
+                x = (leftX - (TeleOpConfig.STICK_DEAD_ZONE * leftX / Math.abs(leftX))) / (1.0 - TeleOpConfig.STICK_DEAD_ZONE);
+                x = -(x) * powerMultiplier;
             } else {
                 x = 0;
             }
