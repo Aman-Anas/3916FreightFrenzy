5ff6c01c6e1beb11be8223166d231d902a61fd6e
Aman Anas <IA.AmanAnas@gmail.com>
2021-10-23 14:13:11 -0400

    Use our own custom field images in MeepMeep

--- MeepMeepVisualizer/src/main/java/com/example/meepmeepvisualizer/MeepMeepVisualizer.java
+++ MeepMeepVisualizer/src/main/java/com/example/meepmeepvisualizer/MeepMeepVisualizer.java
@@ -5,9 +5,19 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MeepMeepVisualizer {
+
     public static void main (String args[]){
+        Image lightFieldImage = null;
+        Image darkFieldImage = null;
 
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
@@ -35,9 +45,23 @@ public class MeepMeepVisualizer {
                                 .build()
                 );
 
+        try {
+            lightFieldImage = ImageIO.read(new File("ffLight.png"));
+            darkFieldImage = ImageIO.read(new File("ffDark.png"));
+        } catch (IOException e) {
+            //basically do nothing in case of error
+            assert true;
+        }
+
+
         mm
                 // Set field image
-                .setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+
+                //.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_OFFICIAL) //Default
+                //.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK) //Default Dark
+
+                .setBackground(darkFieldImage) //Custom Dark
+                //.setBackground(lightFieldImage) //Custom Light
 
                 // Set theme
                 .setTheme(new ColorSchemeRedDark())
